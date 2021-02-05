8021f811b1f279e99041516c0b2f43939dc8d9d0
Rahul <rahul.mishra@anchanto.com>
2021-02-05 12:20:54 +0530

    Added what is rails

--- first.rb
+++ first.rb
@@ -1,1 +1,4 @@
-This is my first file
+What's Rails?
+Rails is a web-application framework that includes everything needed to create database-backed web applications according to the Model-View-Controller (MVC) pattern.
+
+Understanding the MVC pattern is key to understanding Rails. MVC divides your application into three layers: Model, View, and Controller, each with a specific responsibility.
