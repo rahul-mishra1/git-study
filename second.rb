cf59ab48d7b1fae8bda166efcf2745a79c27d7ab
Rahul <rahul.mishra@anchanto.com>
2021-02-05 13:26:21 +0530

    Revert "replace you with Everyone"
    This reverts commit 3cb840c5f1ac65189ed06b3eb1b6eba3968dbcb8.

--- second.rb
+++ second.rb
@@ -1,5 +1,5 @@
 Contributing
-We encourage Everyone to contribute to Ruby on Rails! Please check out the Contributing to Ruby on Rails guide for guidelines about how to proceed. Join us!
+We encourage you to contribute to Ruby on Rails! Please check out the Contributing to Ruby on Rails guide for guidelines about how to proceed. Join us!
 
 Trying to report a possible security vulnerability in Rails? Please check out our security policy for guidelines about how to proceed.
 
